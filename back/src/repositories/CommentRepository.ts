22d09f4973b0107e755355f414265bfe1ddb6376
michael-fasst <93589687+michael-fasst@users.noreply.github.com>
2022-01-20 15:03:24 +0100

    restart project (#8)

--- back/src/repositories/CommentRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import Comment from '../models/Comment.model.js';
-
-@EntityRepository(Comment)
-class CommentRepository extends Repository<Comment> {}
-
-export default CommentRepository;
