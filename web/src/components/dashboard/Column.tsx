e2efdef24e51d55d1dfc064973e1433b22232d66
michael-fasst <michael.bouchelot@fasst.io>
2022-01-21 11:30:09 +0100

    css dashboard

--- web/src/components/dashboard/Column.tsx
+++ web/src/components/dashboard/Column.tsx
@@ -10,10 +10,10 @@ type Props = {
 const Column = ({ title, ticket }: Props) => {
   return (
     <>
-      <main className='min-w-0 flex-1 border-gray-200 lg:flex mr-8 ml-8'>
+      <main className='min-w-0 flex-1 border-gray-200 lg:flex mr-12 ml-12 mt-12'>
         <section
           aria-labelledby='primary-heading'
-          className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-last border border-gray-100 rounded-t-lg'
+          className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-last border border-gray-100 bg-gray-100 rounded-t-lg'
         >
           <h1 className='flex justify-center border-b bg-indigo-600 rounded-t-lg text-white'>
             {title}
