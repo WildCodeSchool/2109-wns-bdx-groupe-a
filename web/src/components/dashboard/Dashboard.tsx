8d9f16d6ccdbe674110c0334c213b187e990ce3e
michael-fasst <michael.bouchelot@fasst.io>
2022-02-21 13:59:21 +0100

    wip

--- web/src/components/dashboard/Dashboard.tsx
+++ web/src/components/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { gql, useQuery } from '@apollo/client';
 import Column from './Column';
 import Header from './Header';
 import LeftMenu from './LeftMenu';
@@ -5,8 +6,36 @@ import LeftMenu from './LeftMenu';
 import { COLUMNS_TICKETS } from './dashboard.constants';
 import { useState } from 'react';
 
+interface Tasks {
+  title: string;
+  description: string;
+  attachment: string;
+  progress_state: string;
+  id: string;
+}
+
+interface TasksData {
+  tasks: Tasks[];
+}
+
+const GET_TASKS = gql`
+  query getTasks {
+    tasks {
+      title
+      description
+      attachment
+      progress_state
+      id
+    }
+  }
+`;
+
 const Dashboard = () => {
   const [columns, setColumns] = useState(COLUMNS_TICKETS);
+
+  const { loading, error, data } = useQuery<TasksData>(GET_TASKS);
+
+  console.log(data);
 
   return (
     <div className='h-full flex flex-col'>
