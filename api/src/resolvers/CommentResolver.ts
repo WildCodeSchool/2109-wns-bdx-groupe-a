89eb22bea7085b4171a04bd9bb3f746ba3667dbb
Mathilde <mathilde.vandewoestyne@scalian.com>
2022-04-28 16:32:21 +0200

    Test test

--- api/src/resolvers/CommentResolver.ts
+++ api/src/resolvers/CommentResolver.ts
@@ -15,7 +15,7 @@ export default class CommentResolver {
 
     @Query(() => String) 
     getTest2() {
-        return 'coucou mise à jour trop cool 23'
+        return 'si ça fonctionne c\'est champagne'
     }
 
     @Mutation(() => Comment)
