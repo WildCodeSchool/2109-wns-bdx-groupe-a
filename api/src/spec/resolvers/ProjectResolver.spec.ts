b77e5996a98a2eb6025ef3526d0adb2dd404f94c
Michaël <michael.bouchelot@fasst.io>
2022-09-04 16:54:36 +0200

    wip

--- api/src/spec/resolvers/ProjectResolver.spec.ts
+++ api/src/spec/resolvers/ProjectResolver.spec.ts
@@ -7,7 +7,7 @@ import getExpressServer from '../../express-server';
 const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS);
 
-describe.skip('ProjectResolver', () => {
+describe('ProjectResolver', () => {
   let testClient: createTestClient.SuperTest<createTestClient.Test>;
 
   beforeAll(async () => {
