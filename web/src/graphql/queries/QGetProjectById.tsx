715d253c5f2118f0a70c6ba43de20f37ed01f341
Michaël <michael.bouchelot@fasst.io>
2022-09-04 22:19:48 +0200

    fix various bug

--- web/src/graphql/queries/QGetProjectById.tsx
+++ web/src/graphql/queries/QGetProjectById.tsx
@@ -1,9 +1,9 @@
 import { gql } from '@apollo/client';
 
 export const GET_PROJECT_BY_ID = gql`
-  query GetProjectById($getProjectById: String!) {
-    getProjectById(id: $getProjectById) {
-      title
-    }
+  query GetProjectById($project: String!) {
+    getProjectById(project: $project) {
+    title
+  }
   }
 `;
